f8a58bc91886cf281a4ec9f7a376398239f61f58
Nina Lofik <ninalofik.dev@gmail.com>
2025-06-01 16:05:18 +0300

    Добавлены новые примеры ввода и обработки целых чисел на С# (Stepik)

--- LearningTopics/00_Basics/integer_input_examples_stepik.cs
+++ LearningTopics/00_Basics/integer_input_examples_stepik.cs
@@ -1,4 +1,4 @@
-// Примеры ввода целых чисел на C#,
+// Примеры ввода и обработки целых чисел на C#,
 // написанные в процессе прохождения курса (Stepik).
 // Все примеры объединены в один класс
 // для удобства тестирования.
@@ -14,6 +14,7 @@ public class IntegerInputExamplesStepik
         Console.WriteLine("Пример 1: квадрат числа");
         Console.Write("Введите целое число: "); 
         int number = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(); // пустая строка для отделения вывода
         Console.WriteLine($"Квадрат числа: {number * number}");
 
         Console.WriteLine(); // пустая строка для отделения примеров
@@ -23,12 +24,38 @@ public class IntegerInputExamplesStepik
         Console.Write("Введите количество метров: ");    
         int meters = Convert.ToInt32(Console.ReadLine());
         int kilometers = meters / 1000;
+        Console.WriteLine();
         Console.WriteLine($"Полных километров: {kilometers}");
 
         Console.WriteLine();
 
-        // Пример 3: 
+        // Пример 3: Произведение трёх чисел
+        Console.WriteLine("Пример 3: произведение трёх чисел");
+        Console.Write("Введите первое число: ");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите второе число: ");
+        int number2 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите третье число: ");
+        int number3 = Convert.ToInt32(Console.ReadLine());
+        int product = number1 * number2 * number3;
+        Console.WriteLine();
+        Console.WriteLine($"Произведение: {product}");
 
+        Console.WriteLine();
+
+        // Пример 4: Делим яблоки поровну между школьниками
+        Console.WriteLine("Пример 4: Делим яблоки поровну между школьниками");
+        Console.Write("Введите количество школьников: ");
+        int students = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите количество яблок: ");
+        int apples = Convert.ToInt32(Console.ReadLine());
+        int applesPerStudent = apples / students;
+        Console.WriteLine();
+        Console.WriteLine($"Каждому школьнику достанется: {applesPerStudent}");
+
+        Console.WriteLine();
+
+        // Пример 5:
       
     }
 }
