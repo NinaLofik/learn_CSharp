f601879ec56f372d692b35dac8ea43ef5ab2b93f
Nina Lofik <ninalofik.dev@gmail.com>
2025-06-03 15:30:31 +0300

    Добавлен в файл пример для символьных литералов на C# (Microsoft Learn)

--- LearningTopics/00_Basics/character_examples_microsoftlearn.cs
+++ LearningTopics/00_Basics/character_examples_microsoftlearn.cs
@@ -1,1 +1,24 @@
+// Примеры для символьных литералов на C#,
+// написанные в процессе прохождения курса (Microsoft Learn).
+// Все примеры объединены в один класс
+// для удобства тестирования.
 
+using System;
+
+public class floatingPointDoubleExamplesStepik
+{
+    public static void Main()
+    {
+      
+      // Пример 1: символьный литерал
+      Console.WriteLine("Пример 1: символьный литерал");
+
+      char letter = 'A'; // литерал одного символа заключён в одинарные кавычки
+      Console.WriteLine($"Символ: {letter}");
+
+      char symbol = '#'; // любой символ, включая спец.символы
+      Console.WriteLine($"Символ: {symbol}");
+      
+      Console.WriteLine(); // пустая строка для отделения вывода
+    }
+}
