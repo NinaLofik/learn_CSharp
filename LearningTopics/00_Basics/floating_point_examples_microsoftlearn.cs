c4b4516c8a016bf479614094940927ff2e4538a5
Nina Lofik <ninalofik.dev@gmail.com>
2025-06-04 12:27:37 +0300

    Добавлены примеры в файл

--- LearningTopics/00_Basics/floating_point_examples_microsoftlearn.cs
+++ LearningTopics/00_Basics/floating_point_examples_microsoftlearn.cs
@@ -9,7 +9,6 @@ public class floatingPointExamplesMicrosoftLearn
 {
     public static void Main()
     {
-
         // Пример 1: тип float (одинарная точность)
         Console.WriteLine("Пример 1: тип float (одинарная точность)");
         // Чтобы создать float литерал, добавьте после числа букву F или f
@@ -47,6 +46,34 @@ public class floatingPointExamplesMicrosoftLearn
         decimal decimalRounded = 1m / 3m;
         Console.WriteLine($"1 / 3 (float): {floatRounded}");
         Console.WriteLine($"1 / 3 (decimal): {decimalRounded}");
+        Console.WriteLine();
+
+        // Пример 6: тип decimal
+        Console.WriteLine("Пример 6: тип decimal");
+        decimal decimalQuotient = 7.0m / 5;
+        // decimal decimalQuotient = 7 / 5.0m; аналогичный способ записи
+        // decimal decimalQuotient = 7.0m / 5.0m; аналогичный способ записи
+        Console.WriteLine($"Decimal quotient: {decimalQuotient}"); // output: Decimal quotient: 1.4
+        Console.WriteLine();
+        // !!!Следующие строки кода не будут работать или будут давать неточные результаты
+        // !!! int decimalQuotient = 7 / 5.0m;
+        // !!! int decimalQuotient = 7.0m / 5;
+        // !!! int decimalQuotient = 7.0m / 5.0m;
+        // !!! decimal decimalQuotient = 7 / 5;
+
+        // Пример 7: добавление кода для приведения результатов целочисленного деления
+        Console.WriteLine("Пример 7: добавление кода для приведения результатов целочисленного деления");
+        int first = 7;
+        int second = 5;
+        decimal quotient = (decimal)first / (decimal)second;
+        Console.WriteLine(quotient); // output: 1.4
+        Console.WriteLine();
+
+        // Пример 8: определение оставшейся части после целочисленного деления
+        Console.WriteLine("Пример 8: определение оставшейся части после целочисленного деления");
+        Console.WriteLine($"Modulus of 200 / 5 : {200 % 5}"); // output: Modulus of 200 / 5 : 0
+        Console.WriteLine($"Modulus of 7 / 5 : {7 % 5}");     // output: Modulus of 7 / 5 : 2
+        Console.WriteLine();
     }
 }
 
